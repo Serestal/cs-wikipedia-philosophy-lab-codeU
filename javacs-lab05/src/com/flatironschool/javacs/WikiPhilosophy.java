e3e3b54953c6fe63e725375ecb088ab77664fae7
Ge Zhang <gz74@cornell.edu>
2016-07-31 21:55:27 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -13,6 +13,7 @@ import org.jsoup.select.Elements;
 public class WikiPhilosophy {
 	
 	final static WikiFetcher wf = new WikiFetcher();
+	static int num_parens = 0;
 	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
@@ -28,24 +29,61 @@ public class WikiPhilosophy {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
+		String url =  getFirstValidLinkOnPage("https://en.wikipedia.org/wiki/Java_(programming_language)");
+		while (!url.equals("https://en.wikipedia.org/wiki/Philosophy")){
+			if (url.equals("")) return;
+			System.out.println(url);
+			url = getFirstValidLinkOnPage(url);
+		}
+		System.out.println(url);
+		return;
+	}
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+	public static String getFirstValidLinkOnPage(String url) throws IOException {
+		//reset parenthesis count
+		num_parens = 0;
 		Elements paragraphs = wf.fetchWikipedia(url);
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		// for each paragraph on the page
+		for (Element paragraph : paragraphs) {
+			// iterate through the dom tree
+			Iterable<Node> iter = new WikiNodeIterable(paragraph);
+			for (Node node: iter) {
+				if (node instanceof TextNode) {
+					// update num_parenthesis
+					for (char character : ((TextNode) node).text().toCharArray()) {
+						if (character == '(') num_parens ++;
+						else if (character == ')') num_parens --;
+					}
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+					if (isValidLink((TextNode) node)) {
+						String linked_url = ((Element)node.parent()).attr("abs:href");
+						// if this link is not the current link, return it!
+						if (!linked_url.equals(url) && !linked_url.contains(url + "#")) return linked_url;
+					}
+				}
+	        }
+	    }
+	    System.out.println("Returning a nil");
+  		return "";
+	}
+
+	/** 
+	 *	Given a text node, returns true if it is a link that is not 
+	 * 	in italics, parenthesis, or links to the current page.
+	**/
+
+	public static boolean isValidLink(TextNode node) {
+		Element parent = (Element) node.parent();
+		return (parent.tagName() == "a" && num_parens <= 0 && !isItalicized(node));
+	}
+
+	public static boolean isItalicized(TextNode node) {
+		Element parent = (Element) node.parent();
+		if (parent.tagName() == "i" || parent.tagName() == "em") return true;
+		for (Element ancestor : parent.parents()) {
+			if (ancestor.tagName() == "i" || ancestor.tagName() == "em") return true;
+		}
+		return false;
 	}
 }
